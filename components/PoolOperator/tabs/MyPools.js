bb87b5794607b5c0b593ac2dfd963c7d2d1d8d2e
alonavns <alona.vns@outlook.com>
2020-01-13 10:03:31 +0200

    pool actions

--- components/PoolOperator/tabs/MyPools.js
+++ components/PoolOperator/tabs/MyPools.js
@@ -1,6 +1,11 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import Table from '~/components/common/Table'
-// import Modal from '~/components/common/Modal'
+import Modal from '~/components/common/Modal'
+
+const Wrapper = styled.div`
+  text-align: left;
+`
 
 const headers = [
   {
@@ -51,7 +56,7 @@ const actions = [
   {
     label: 'Withdraw earnings',
     slot: 'withdraw',
-    visible: ({ unusedContributions }) => unusedContributions > 0,
+    // visible: ({ unusedContributions }) => unusedContributions > 0,
   },
   {
     label: 'Close Pool',
@@ -59,23 +64,162 @@ const actions = [
   },
 ]
 
+const formFields = {
+  offer: (defaults, options = {}) => ({
+    fields: [
+      {
+        key: 'expiry',
+        label: 'Expiry',
+        required: ({ expiry }) => !!expiry,
+        type: 'select',
+        autoFocus: true,
+        options: options.expiries,
+        noNoneValue: true,
+        valueKey: 'name',
+        labelValue: ({ name, date }) => `${name} (${date})`,
+      },
+      ...(options.riskFree
+        ? []
+        : [
+            {
+              key: 'underlying',
+              label: 'Underlying',
+              required: ({ underlying, strike }) =>
+                (underlying && Number(strike)) ||
+                (!underlying && !Number(strike)),
+              type: 'select',
+              options: options.underlying,
+            },
+            {
+              key: 'strike',
+              label: 'Strike Price',
+              required: ({ underlying, strike }) =>
+                (underlying && Number(strike)) ||
+                (!underlying && !Number(strike)),
+              type: 'number',
+            },
+          ]),
+      {
+        key: 'iCostPerDay',
+        label: 'I Cost per day',
+        type: 'number',
+      },
+      ...(options.riskFree
+        ? []
+        : [
+            {
+              key: 'sCostPerDay',
+              label: 'S Cost per day',
+              type: 'number',
+            },
+          ]),
+    ],
+    defaults,
+  }),
+}
+
 export default function MyPools({
+  library,
   riskFree,
   riskFreePools,
   riskyPools,
-  ...props
 }) {
-  const handleAction = (...args) => {
-    console.log(args)
-  }
+  const { balanceTokens, expiries } = library.contracts
+  const currencies = balanceTokens.filter(t => t !== 'LST')
   const data = (riskFree ? riskFreePools : riskyPools) || []
+  const [modal, setModal] = useState(null)
+
+  const fetchPools = () => {
+    if (riskFree) {
+      library.contracts.fetchRiskFreePools()
+    } else {
+      library.contracts.getRiskyPools()
+    }
+  }
+
+  const handleModal = form => {
+    const { poolId, slot } = modal
+    switch (slot) {
+      case 'offer': {
+        const { expiry, underlying, strike, ...info } = form
+        library.contracts
+          .onOfferNewToken(poolId, {
+            expiry,
+            underlying,
+            strike: Number(strike),
+            ...info,
+          })
+          .then(() => {
+            fetchPools()
+            setModal(null)
+          })
+        break
+      }
+      default:
+        console.log(modal, form)
+        break
+    }
+  }
+
+  const handleAction = async (slot, data) => {
+    switch (slot) {
+      case 'offer': {
+        const { id: poolId } = data
+        setModal({
+          slot,
+          poolId,
+          data: formFields.offer(
+            {
+              expiry: expiries[0].timestamp,
+              strike: '0',
+              iCostPerDay: 0.025,
+              sCostPerDay: 0.05,
+            },
+            {
+              underlying: currencies,
+              expiries,
+              riskFree,
+            }
+          ),
+        })
+        break
+      }
+      case 'withdraw': {
+        const { id: poolId } = data
+        library.contracts.onWithdrawEarnings(poolId, riskFree).then(() => {
+          fetchPools()
+        })
+        break
+      }
+      case 'close': {
+        const { id: poolId } = data
+        library.contracts.onClosePool(poolId, riskFree).then(() => {
+          fetchPools()
+        })
+        break
+      }
+      default:
+        console.log(slot, data)
+        break
+    }
+  }
 
   return (
-    <Table
-      headers={headers}
-      data={data}
-      actions={actions}
-      onAction={handleAction}
-    />
+    <Wrapper>
+      <Table
+        headers={headers}
+        data={data}
+        actions={actions}
+        onAction={handleAction}
+      />
+      {!!modal && (
+        <Modal
+          title="Input Fields"
+          {...modal.data}
+          onSubmit={handleModal}
+          onClose={() => setModal(null)}
+        />
+      )}
+    </Wrapper>
   )
 }
